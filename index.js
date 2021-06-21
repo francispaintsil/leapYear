cdc3c5127ae9bfdc125113007acacd25d3b3a12e
Francis <francis.paintsil@amalitech.org>
2021-06-21 03:19:33 -0700

    update innertext

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ let year= document.getElementById("yearInput");
 let button= document.getElementById("pressButton");
 let outPut = document.getElementById("outPutt");
 button.addEventListener("click", function pressBtn(){
-
+let h1 = document.querySelector("h1");
+h1.innerText = "Welcome! Enter a year"
 let leapBoolean = (year.value % 100 === 0 ? year.value % 400 === 0: year.value % 4 === 0);
 
 if(year.value === '' || year.value === 0){
-    alert('Enter a valid year');
+    h1.innerText='Enter a valid year';
     return;
 }
 if(leapBoolean){
 
-    alert(`${year.value} is a leap Year!`);
+    h1.innerText=`${year.value} is a leap Year!`;
 }else{
-    alert(`${year.value} not a leap year`);
+    h1.innerText=`${year.value} not a leap year`;
 }
 
 })
